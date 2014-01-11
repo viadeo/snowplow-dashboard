a006758ea7c28787a9c3f78dd172e9a4578195ec
Eric Pantera <eric.pantera@gmail.com>
2014-01-11 17:13:26 +0100

    Init Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SnowplowDashboard::Application.config.secret_key_base = '1c952ac14b9a92acc9a19bdd08e4a6614edd87be4980cdca5d96af84b6f2a08cd1596db0e7934f400c05a8d198f7986b702e89f420c7b886af3a895656043232'
