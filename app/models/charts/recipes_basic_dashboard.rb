afb1899a6ba98517a279cdf6b04ed16bef6807af
Eric Pantera <eric.pantera@gmail.com>
2014-01-11 20:47:54 +0100

    Add RecipesBasic wrapper

---
+++ app/models/charts/recipes_basic_dashboard.rb
@@ -0,0 +1,75 @@
+class RecipesBasicDashboard
+
+	def pages_per_visit
+		Event.from_sql("select * from recipes_basic.pages_per_visit")
+	end
+
+	def behavior_frequency 
+		Event.from_sql("select * from recipes_basic.behavior_frequency")
+	end
+
+	def behavior_recency 
+		Event.from_sql("select * from recipes_basic.behavior_recency")
+	end
+
+	def bounce_rate_by_day 
+		Event.from_sql("select * from recipes_basic.bounce_rate_by_day")
+	end
+	
+	def engagement_pageviews_per_visit 
+		Event.from_sql("select * from recipes_basic.engagement_pageviews_per_visit")
+	end
+	
+	def avg_visit_duration_by_day 
+		Event.from_sql("select * from recipes_basic.avg_visit_duration_by_day")
+	end
+	
+	def engagement_visit_duration 
+		Event.from_sql("select * from recipes_basic.engagement_visit_duration")
+	end
+	
+	def events_by_day 
+		Event.from_sql("select * from recipes_basic.events_by_day")
+	end
+	
+	def fraction_new_visits_by_day 
+		Event.from_sql("select * from recipes_basic.fraction_new_visits_by_day")
+	end
+	
+	def new_vs_returning 
+		Event.from_sql("select * from recipes_basic.new_vs_returning")
+	end
+	
+	def pages_per_visit 
+		Event.from_sql("select * from recipes_basic.pages_per_visit")
+	end
+	
+	def pageviews_by_day 
+		Event.from_sql("select * from recipes_basic.pageviews_by_day")
+	end
+	
+	def technology_browser 
+		Event.from_sql("select * from recipes_basic.technology_browser")
+	end
+	
+	def technology_mobile 
+		Event.from_sql("select * from recipes_basic.technology_mobile")
+	end
+	
+	def technology_os 
+		Event.from_sql("select * from recipes_basic.technology_os")
+	end
+	
+	def uniques_and_visits_by_day 
+		Event.from_sql("select * from recipes_basic.uniques_and_visits_by_day")
+	end
+	
+	def visitors_by_language 
+		Event.from_sql("select * from recipes_basic.visitors_by_language")
+	end
+	
+	def visits_by_country 
+		Event.from_sql("select * from recipes_basic.visits_by_country")
+	end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module SnowplowDashboard
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(
-        #{config.root}/app/models/events        
+      #{config.root}/app/models/events
+      #{config.root}/app/models/charts
     )        
   end
 end
