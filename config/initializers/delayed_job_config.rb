8e868678409d7b2e2c517d5483b06f99531979be
Eric Pantera <eric.pantera@gmail.com>
2014-01-18 23:43:03 +0100

    Add database.yml and remove unused delay_job initializer

--- config/initializers/delayed_job_config.rb
+++
@@ -1,5 +0,0 @@
-# Use a dedicated database for delay_job to prevent using the redshift instance in production
-
-Delayed::Job.class_eval do
-  establish_connection ActiveRecord::Base.configurations["#{Rails.env}"]
-end
