04da9402c0798e738c32b0bb39c8228e4b758dfe
Eric Pantera <eric.pantera@gmail.com>
2014-01-28 23:24:41 +0100

    Load last Redshift event time in background

--- app/models/charts/welcome_dashboard.rb
+++ app/models/charts/welcome_dashboard.rb
@@ -1,6 +1,12 @@
 class WelcomeDashboard < ChartDashboard
 
+	refresh :last_event_date, every: 10.minutes
 	refresh :events_count_per_day, every: 1.hour
+
+
+	def last_event_date
+		{ "Last event in RedShift: " => Event.last_update }
+	end
 
 	def events_count_per_day
 		Event.from_sql("select collector_tstamp::date, count(*) from events group by 1 order by 1;")
